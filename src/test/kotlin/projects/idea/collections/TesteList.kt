74e70b30923dc1fa06667572676332454f4c4a3e
Luis Leda <luis_neto@live.com>
2022-08-05 00:28:25 -0300

    commit Desenvolvimento com listOf

---
+++ src/test/kotlin/projects/idea/collections/TesteList.kt
@@ -0,0 +1,26 @@
+package projects.idea.collections
+
+fun main() {
+    val EGMA = Empresa("EGMA", "11.308.791/0001-38")
+    val SEATI = Empresa("SEATI", "05.022.633/0001-14")
+    val SECTI = Empresa("SECTI", "05.572.043/0001-65")
+
+    val empresas = listOf(EGMA, SEATI, SECTI)
+
+    empresas.forEach { println(it) }
+    println("0101010101010101")
+
+    println(empresas.find { it.nome == "SEATI" })
+
+}
+
+data class Empresa(
+    val nome: String,
+    val cnpj: String
+) {
+    override fun toString(): String =
+        """
+            Nome: $nome
+            CNPJ: $cnpj
+        """.trimIndent()
+    }
