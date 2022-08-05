a55d8b4b43608e6fba51fa55e57ab99cdcfc60a5
Luis Leda <luis_neto@live.com>
2022-08-05 02:33:13 -0300

    commit funçoes estendidas

---
+++ src/test/kotlin/projects/idea/collections/TestFuncoesEstendidas.kt
@@ -0,0 +1,13 @@
+package projects.idea.collections
+
+fun main() {
+    val salarios = arrayOf(
+        "2000".toBigDecimal(),
+        "2500".toBigDecimal(),
+        "3500".toBigDecimal()
+    )
+    println("01010101010101101010")
+    println(salarios.somatoria())
+    println("01010101010101101010")
+    println(salarios.media())
+}
